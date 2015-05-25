1116a15533cc340593630eed62e399b459855632
Terry Chia <chiawz@hotmail.com>
2015-05-26 00:55:13 +0800

    Upload everything

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # substring
 Search for substring inside string
+Created by : Terry Chia
+
+Write the following function in C.
+
+stripos — Find position of first occurrence of a case-insensitive string
+int stripos ( char* haystack, char* needle, int offset )
+
+Returns the numeric position of the first occurrence of needle in the haystack string. Note that the needle may be a string of one or more characters. If needle is not found, stripos() will return -1.
+
+The function should not make use of any C library function calls. The code should be developed entirely by you and not extracted from other's work.
+
+Please ensure that your email with any attachment is less than 200KB.
---
+++ opensource.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <string.h>
+
+	int stripos (char *haystack, char *needle, int offset ){
+
+		int count=0,i,needlecount=0,j=0;
+
+		// counting length of haystack
+		do{
+			count++;
+		}
+		while(*haystack++);
+
+		haystack=haystack - count;
+
+		//Counting length of needle
+		do{	
+			needlecount++;
+		}
+		while(*needle++);
+		needle = needle - needlecount;
+
+		/*Comparison*/
+		for(i=0; i<= count;i++){
+			
+			if(*(haystack+offset+i) == *(needle+j)){ //If charc of haystack and needle is the same. Offset is taken into consideration. 
+					j++;
+					if(j == (needlecount-1)) {
+							printf("Total char counted =%d\n", i+1);
+							printf("Length of needle = %d\n", j);
+							printf("Offset value = %d\n",offset);
+							return (i+1-j);
+							break;
+						}	
+				}
+			else{j=0;} //If is not the same - Reset J
+		}
+	return -1; //needle not found
+	}   
+
+
+int main()
+{	int position = stripos("helloworld", "or" , 0);
+	if (position == -1) {
+		printf("Needle is not found in haystack.\n");
+	} 
+	else {
+		printf("Position of needle in haystack = %d\n",position );
+	}
+}
